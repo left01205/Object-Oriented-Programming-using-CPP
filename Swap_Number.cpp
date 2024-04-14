e033f10e6771edd6b5807d44c94364365ae1f623
Arnav Jha <91931013+left01205@users.noreply.github.com>
2024-04-14 13:55:02 +0530

    Create Swap_Number.cpp

---
+++ Swap_Number.cpp
@@ -0,0 +1,33 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+class swap_no
+{
+    private:
+    int a,b;
+    public:
+    void inp()
+    {
+        cout<<"Enter number 'a' and 'b'= ";
+        cin>>a>>b;
+        cout<<"Numbers before swapping are "<<"a="<<a<<"b="<<b;
+    }
+    void swap()
+    {
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+    void out()
+    {
+        cout<<"Numbers after swapping are "<<"a="<<a<<"b="<<b;
+    }
+};
+int main()
+{
+    swap_no sn;
+    sn.inp();
+    sn.swap();
+    sn.out();
+    return 0;
+}
